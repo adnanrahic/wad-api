5646f7bfa5a9be99ecbbb2b9d93b44dcc9822492
adnanrahic <ado.raha198@gmail.com>
2018-05-22 10:49:57 +0200

    app tweaks

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 require('./db')
 
-const noteController = require('./notes/note.controller')
-
 app.get('/api', (req, res, next) => {
   res.status(200).send('Api Works.')
 })
 
+const noteController = require('./notes/note.controller')
 app.use('/api/notes', noteController)
 
 app.listen(3000, () => console.log('Server is running on port 3000'))
